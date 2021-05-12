f2a9a5dc58462ecb64bb8d5614653d150d6d0d54
acdn-ekeddy <32712164+acdn-ekeddy@users.noreply.github.com>
2021-05-12 14:12:37 -0400

    Update plugin

--- src/main/scala/com/accedian/npav/CustomLoggingPlugin.scala
+++ src/main/scala/com/accedian/npav/CustomLoggingPlugin.scala
@@ -20,17 +20,7 @@ class CustomLoggingPlugin extends SparkPlugin {
 class CustomExecutorLoggingPlugin extends ExecutorPlugin with Logging {
 
   override def init(ctx: PluginContext, extraConf: util.Map[String, String]): Unit = {
-    logInfo("init")
-
-    val thread = new Thread {
-      override def run {
-        while(!SparkLoggingHelper.reconfigureLogging) {
-          Thread.sleep(5000)
-        }
-      }
-    }
-    thread.setDaemon(true)
-    thread.start
+    SparkLoggingHelper.reconfigureLogging
     super.init(ctx, extraConf)
   }
 
@@ -39,7 +29,6 @@ class CustomExecutorLoggingPlugin extends ExecutorPlugin with Logging {
 
 class CustomDriverLoggingPlugin extends DriverPlugin with Logging {
   override def init(sc: SparkContext, pluginContext: PluginContext): util.Map[String, String] = {
-    logInfo("init")
     SparkLoggingHelper.reconfigureLogging
     super.init(sc, pluginContext)
   }
@@ -51,17 +40,20 @@ object SparkLoggingHelper extends Serializable with Logging {
   @volatile var reconfigured = false
 
   def reconfigureLogging(): Boolean = synchronized {
-    if (!reconfigured) {
+    if (!reconfigured) try {
 
       val logConfigFilename = System.getProperty("log4j.configuration")
       val absoluteConfigFilename: Option[String] = if (logConfigFilename != null) {
         lazy val files = SparkEnv.get.conf.get("spark.files", "")
         lazy val source = files.split(",").find(_.endsWith(logConfigFilename)).orNull
+
         if (new java.io.File(logConfigFilename).exists) {
+          // The file is already present in the working directory
           println(s"$logConfigFilename exists")
           Some(logConfigFilename)
         }
         else if (source != null) {
+          // Download the file and place it in the working directory
           val dest = SparkFiles.get(logConfigFilename)
           val srcFS = new Path(source).getFileSystem(new Configuration())
           println(s"Copying $source to $dest")
@@ -83,6 +75,10 @@ object SparkLoggingHelper extends Serializable with Logging {
         PropertyConfigurator.configure(f)
         reconfigured = true
       }
+    }
+    catch {
+      case ex:Throwable =>
+        logWarning("Failed to configure logging",ex)
     }
     reconfigured
   }
